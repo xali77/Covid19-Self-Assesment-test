f6acd124406a7cc7152486c72b4af4a2c565e1fa
Mho Ali <mh.ali5677@gmail.com>
2020-10-04 12:47:24 +0530

    Final

---
+++ sc.js
@@ -0,0 +1,240 @@
+
+    var ma=0;
+    var c1=0;
+    var c2=0;
+    var c3=0;
+    var c4=0;
+    var c5=0;
+    var c6=0;
+    var c7=0;
+    var c8=0;
+    var c9=0;
+    var c10=0;
+    var c11=0;
+    var c12=0;
+    var c13=0;
+    var c14=0;
+    
+    function fev(){
+        c1=c1+1;
+        if(c1%2==0){
+            ma=ma-3;
+            document.querySelector("button.butt").style.background="#EFEFEF";
+        }
+        else if (c1%2==1)
+        {
+            document.querySelector("button.butt").style.background="#D94D4C";
+            ma=ma+3;
+        }
+
+        console.log(ma)
+        }
+
+        function cou(){
+        c2=c2+1;
+        if(c2%2==0){
+            ma=ma-4;
+            document.querySelector("button.butt1").style.background="#EFEFEF";
+        }
+        else if (c2%2==1)
+        {
+            document.querySelector("button.butt1").style.background="#D94D4C";
+            ma=ma+4;
+            
+        }
+        console.log(ma)
+        }
+        
+
+        function dif(){
+        c3=c3+1;
+        if(c3%2==0){
+            ma=ma-6;
+            document.querySelector("button.butt2").style.background="#EFEFEF";
+        }
+        else if (c3%2==1)
+        {
+            ma=ma+6;
+            document.querySelector("button.butt2").style.background="#D94D4C";
+        }
+        console.log(ma)
+        }
+
+
+        function dia(){
+        c4=c4+1;
+        if(c4%2==0){
+            ma=ma-2;
+            document.querySelector("button.buttt2").style.background="#EFEFEF";
+        }
+        else if (c4%2==1)
+        {
+            ma=ma+2;
+            document.querySelector("button.buttt2").style.background="#D94D4C";
+        }
+        console.log(ma)
+        }
+
+
+
+        function loss(){
+        c5=c5+1;
+        if(c5%2==0){
+            ma=ma-7;
+            document.querySelector("button.butt3").style.background="#EFEFEF";
+        }
+        else if (c5%2==1)
+        {
+            ma=ma+7;
+            document.querySelector("button.butt3").style.background="#D94D4C";
+        }
+        console.log(ma)
+        }
+
+
+
+        function uu(){
+        c6=c6+1;
+        if(c6%2==0){
+            ma=ma-1;
+            document.querySelector("button.butt4").style.background="#EFEFEF";
+        }
+        else if (c6%2==1)
+        {
+            ma=ma+1;
+            document.querySelector("button.butt4").style.background="#D94D4C";
+        }
+        console.log(ma)
+        }
+
+        function diab(){
+        c7=c7+1;
+        if(c7%2==0){
+            ma=ma-3;
+            document.querySelector("button.butt6").style.background="#EFEFEF";
+        }
+        else if (c7%2==1)
+        {
+            ma=ma+3;
+            document.querySelector("button.butt6").style.background="#D94D4C";
+        }
+        console.log(ma)
+        }
+
+
+        function bp(){
+        c8=c8+1;
+        if(c8%2==0){
+            ma=ma-3;
+            document.querySelector("button.butt7").style.background="#EFEFEF";
+        }
+        else if (c8%2==1)
+        {
+            ma=ma+3;
+            document.querySelector("button.butt7").style.background="#D94D4C";
+        }
+        console.log(ma)
+        }
+
+
+        function ld(){
+        c9=c9+1;
+        if(c9%2==0){
+            ma=ma-4;
+            document.querySelector("button.butt8").style.background="#EFEFEF";
+        }
+        else if (c9%2==1)
+        {
+            ma=ma+4;
+            document.querySelector("button.butt8").style.background="#D94D4C";
+        }
+        console.log(ma)
+        }
+
+
+        function can(){
+        c10=c10+1;
+        if(c10%2==0){
+            ma=ma-3;
+            document.querySelector("button.buttt8").style.background="#EFEFEF";
+        }
+        else if (c10%2==1)
+        {
+            ma=ma+3;
+            document.querySelector("button.buttt8").style.background="#D94D4C";
+        }
+        console.log(ma)
+        }
+
+
+        function hd(){
+        c11=c11+1;
+        if(c11%2==0){
+            ma=ma-4;
+            document.querySelector("button.butt9").style.background="#EFEFEF";
+        }
+        else if (c11%2==1)
+        {
+            ma=ma+4;
+            document.querySelector("button.butt9").style.background="#D94D4C";
+        }
+        console.log(ma)
+        }
+
+
+        function kd(){
+        c12=c12+1;
+        if(c12%2==0){
+            ma=ma-3;
+            document.querySelector("button.butt10").style.background="#EFEFEF";
+        }
+        else if (c12%2==1)
+        {
+            ma=ma+3;
+            document.querySelector("button.butt10").style.background="#D94D4C";
+        }
+        console.log(ma)
+        }
+
+
+        function trav(){
+        c13=c13+1;
+        if(c13%2==0){
+            ma=ma-6;
+            document.querySelector("button.butt11").style.background="#EFEFEF";
+        }
+        else if (c13%2==1)
+        {
+            ma=ma+6;
+            document.querySelector("button.butt11").style.background="#D94D4C";
+        }
+        console.log(ma)
+        }
+
+        function posi(){
+        c14=c14+1;
+        if(c14%2==0){
+            ma=ma-8;
+            document.querySelector("button.butt12").style.background="#EFEFEF";
+        }
+        else if (c14%2==1)
+        {
+            ma=ma+9;
+            document.querySelector("button.butt12").style.background="#D94D4C";
+        }
+        console.log(ma)
+        }
+
+        function locals(){
+            localStorage.setItem("kk",ma);
+        }
+       
+
+    
+
+
+
+      
+      
+    
+
